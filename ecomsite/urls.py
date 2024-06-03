f2156c841a6985b0ae170349cf3cd256c9cacdc8
Madhuranjan Kumar <madhuranjan763@gmail.com>
2024-06-03 17:55:09 +0530

    Completed the shop app

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('<int:id>/',views.detail,name='detail_view'),
+    path('checkout/',views.checkout,name="checkout"),
 ]
