a932664dc99b780ac803d532c338d5fe513c6330
Madhuranjan Kumar <madhuranjan763@gmail.com>
2024-06-03 18:17:31 +0530

    Customized Admin Panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Products,Order
+
+
+
+
+#changing the admin header
+admin.site.site_header = "E-Commerce Site"
+
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
+class ProductsAdmin(admin.ModelAdmin):
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+
+    list_display = ('title','price','discount_price','category','description','image')
+    search_fields = ['title','category']
+    actions = ('change_category_to_default',)
+    fields = ('title','price','description')
+    list_editable = ('price','discount_price')
+
+
 # Register your models here.
-admin.site.register(Products)
+admin.site.register(Products,ProductsAdmin)
 admin.site.register(Order)
